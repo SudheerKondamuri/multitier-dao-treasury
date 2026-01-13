8cea3ebb1f01b0e7d2620054c2431b1cd54dba77
SudheerKondamuri <sudheersudheer7654321@gmail.com>
2026-01-13 22:56:39 +0530

    fix: path changed in GovernanceToken.sol

--- contracts/token/GovernanceToken.sol
+++ contracts/token/GovernanceToken.sol
@@ -5,7 +5,7 @@ import {ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import {ERC20Permit} from "@openzeppelin/contracts/token/ERC20/extensions/ERC20Permit.sol";
 import {ERC20Votes} from "@openzeppelin/contracts/token/ERC20/extensions/ERC20Votes.sol";
 import {Nones} from "@openzeppelin/contracts/utils/Nones.sol";
-import {VotingPowerMath} from "./libraries/VotingPowerMath.sol";
+import {VotingPowerMath} from "../libraries/VotingPowerMath.sol";
 
 /**
  * @title GovernanceToken
